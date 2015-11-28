8e6e16e8814c3916dcc659058101e3c53c8abbe0
jakeyray18 <jakeyray18@gmail.com>
2015-11-28 15:45:56 -0600

    Fixed call to array with zero length.

--- DeathArena/src/DeathArena/Handlers/RestoreInventory.java
+++ DeathArena/src/DeathArena/Handlers/RestoreInventory.java
@@ -12,17 +12,16 @@ import java.util.List;
 public class RestoreInventory {
 
     ConfigUtils CU = new ConfigUtils();
-
+    @SuppressWarnings("unchecked")
     public void restoreInv(Player player, Plugin plugin) {
         YamlConfiguration playerConf = CU.getCConfig(player, plugin);
         player.getInventory().clear();
         player.getInventory().setArmorContents(null);
         ItemStack[] content;
-
-        content = ((List<ItemStack>) playerConf.get("inventory.armor") ).toArray(new ItemStack[0]);
+        content = ((List<ItemStack>) playerConf.get("inventory.armor") ).toArray(new ItemStack[playerConf.getList("inventory.armor").size()]);
         player.getInventory().setArmorContents(content);
         playerConf.set("inventory.armor", null);
-        content = ((List<ItemStack>) playerConf.get("inventory.content")).toArray(new ItemStack[0]);
+        content = ((List<ItemStack>) playerConf.get("inventory.content")).toArray(new ItemStack[playerConf.getList("inventory.armor").size()]);
         player.getInventory().setContents(content);
         playerConf.set("inventory.content", null);
         CU.saveCConfig(player, playerConf, plugin);
